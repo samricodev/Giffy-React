e351f59e61757918205811a21bc697115920b03c
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-01-13 00:17:39 -0600

    Detail page stylized

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,6 +3,7 @@ import { Redirect } from 'wouter'
 import useSingleGif from '../hooks/useSingleGif'
 import Spinner from '../components/Spinner'
 import Gif from '../components/Gif'
+import '../css/Detail.css'
 
 function Detail({ params }) {
   const {gif, isError, isLoading} = useSingleGif({ id: params.id})
@@ -25,8 +26,10 @@ function Detail({ params }) {
       <title>{gif.title} - Giffy</title>
       <meta name="description" content={`Detail of ${decodeURI(gif.title)}`}/>
     </Helmet>
-    <h3>{gif.title}</h3>
-    <Gif {...gif} />
+    <h3 className='title'>{gif.title}</h3>
+    <div className="singleGif">
+      <Gif {...gif} />
+    </div>
   </>
 }
 
