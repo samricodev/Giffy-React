73f9b7a0c6e0aab5918524876a08f61555060aad
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-01-17 23:20:53 -0600

    fix final version

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { Link, Route, Switch } from 'wouter'
 import { GifsContextProvider } from './context/GifsContext'
 import SearchResults from './pages/SearchResults'
 import ErrorPage from './pages/ErrorPage'
-import Header from './components/Header'
 import Detail from './pages/Detail'
-import Login from './pages/Login'
 import Home from './pages/Home'
 import "./App.css"
 
@@ -12,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <Header />
         <Link to="/">
           <figure className="App-logo">
             <img src="/logo.png" alt="Giffy logo" />
@@ -24,7 +21,6 @@ function App() {
             <Route path="/search/:keyword/:rating?" component={SearchResults} />
             <Route path="/gif/:id" component={Detail} />
             <Route path="/404" component={ErrorPage} />
-            <Route path="/login" component={Login} />
           </Switch>
         </GifsContextProvider>
       </section>
